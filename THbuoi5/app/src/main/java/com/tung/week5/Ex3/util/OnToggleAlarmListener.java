af09a2061286379706844445bbfadde0f96cc644
chetung2808 <chetung2808@gmail.com>
2023-06-19 21:37:53 +0700

    Add lab 5

---
+++ THbuoi5/app/src/main/java/com/tung/week5/Ex3/util/OnToggleAlarmListener.java
@@ -0,0 +1,11 @@
+package com.thanh.week5.Ex3.util;
+
+import android.view.View;
+
+import com.thanh.week5.Ex3.model.Alarm;
+
+public interface OnToggleAlarmListener {
+    void onToggle(Alarm alarm);
+    void onDelete(Alarm alarm);
+    void onItemClick(Alarm alarm, View view);
+}
---
+++ THbuoi5/app/src/main/java/com/tung/week5/Ex4/Exercise4.java
@@ -0,0 +1,6 @@
+package com.thanh.week5.Ex4;
+
+import android.app.Activity;
+
+public class Exercise4 extends Activity {
+}
